41434ca6d83bfda367ed81ef2a7b70c665acc61a
luyvannda <luyvannda@gmail.com>
2024-03-17 16:14:12 +0700

    learned how to read and write to a file using fs module

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const readline = require("readline");
+const fs = require("fs"); //fs stand for "file system"
+
+/* Lecture 4: Reading Input and Writing Output
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -14,3 +17,13 @@ rl.on("close", () => {
   console.log("Interface closed");
   process.exit(0);
 });
+*/
+
+/* Lecture 5: Reading and Writing to a File */
+let textIn = fs.readFileSync("./Files/input.txt", "utf-8");
+console.log(textIn);
+
+// pitfall: read file synchronously will have to wait for an entire file to be read before the console.log can be executed
+
+let content = `Data read from input.txt: ${textIn}. \nDate created ${new Date()}`;
+fs.writeFileSync("./Files/output.txt", content);
